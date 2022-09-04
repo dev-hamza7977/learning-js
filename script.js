2b61c82f1651bb9e70c1987430256426bb1d6e70
dev-hamza7977 <85806118+dev-hamza7977@users.noreply.github.com>
2022-09-05 02:14:58 +0530

    completed by datatypes

---
+++ script.js
@@ -0,0 +1,70 @@
+// let x;
+// x = 75;
+// alert(x);
+// console.log(x);
+
+// let message = "Hello To Dark World !";
+// alert(message);
+// console.log(message);
+
+// var newdata = "Are You Ready For This Adventure !"
+// alert(newdata);
+// console.log(newdata);
+
+// let messagebox;
+// messagebox = "This Is Your First Step";
+// messagebox = "This Is Your Second Step Of The First";
+// messagebox = "This Is Your Third Step Of The First";
+// alert(messagebox);
+// console.log(messagebox);
+
+// let admin;
+// let name;
+// name = "Danny";
+// admin = name;
+// console.log(admin);
+
+// const birthday = "08.01.0000";
+// console.log(birthday);
+
+// Datatypes
+
+// 1. Number datatype
+// let a = 1234;
+// let b = 12.34;
+// alert(typeof (b));
+
+// 2. BigInt Datatype
+// let bn = 1234567890n
+// console.log(typeof (bn))
+
+// 3. String datatype
+// let wish = "good"
+// let goodwishes = "night"
+// console.log(typeof (night))
+// let allwishes = `very ${wish}`;
+// console.log(allwishes);
+// console.log(`all wishes is ${1 + 2}`)
+
+// 4. Boolean datatype
+// let isGreater = 4 > 2;
+// alert(isGreater);
+// console.log(isGreater);
+// alert(typeof (isGreater));
+// console.log(typeof (isGreater));
+
+// 5. Null datatype
+// let age = null;
+// console.log(age);
+
+// 6. Unefine datatype
+// let x;
+// console.log(x);
+
+// 7. Symbol datatype
+// let value = Symbol("maruti");
+// console.log(value);
+
+// 8. Object datatype
+// let car = { colour: "red", company: "maruti", type: "diesel" };
+// console.log(car);
