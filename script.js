27be36cf2a006a54bce63f29634c5d51c4e29548
dev-hamza7977 <85806118+dev-hamza7977@users.noreply.github.com>
2022-09-06 01:21:39 +0530

    remaining array

--- script.js
+++ script.js
@@ -68,3 +68,164 @@
 // 8. Object datatype
 // let car = { colour: "red", company: "maruti", type: "diesel" };
 // console.log(car);
+
+
+// BASIC OPERATORS
+
+// 1.Arethmatic Operators
+
+// addition
+// let a = 20;
+// let b = 30;
+// let c = a + b;
+// alert(c);
+
+// subtraction
+// let a1 = 500;
+// let a2 = 100;
+// let a3 = a1 - a2;
+// alert(a3);
+
+// multiplication
+// let d = 500;
+// let e = 100;
+// let f = d * e;
+// alert(f);
+
+// Exponention
+// let f = 5;
+// let h = 2;
+// let z = f ** h;
+// alert(z);
+
+// Division (quotient)
+// let g = 5;
+// let i = 2;
+// let j = g / i;
+// alert(j);
+
+// Moduldus (remanider)
+// let m = 5;
+// let n = 2;
+// let o = m % n;
+// alert(o);
+
+
+// 2. ASIGNMENT OPERATORS
+
+// =
+// var x = 2;
+// var y = 5;
+// var z = x = y;
+// console.log(z);
+
+// +=
+// a1 = 30;
+// a1 += 20;
+// console.log(a1);
+
+// let n = 10;
+// n += 10; //20
+// n *= 10; //200
+// alert(n); //200
+
+// -=
+// a1 = 1;
+// a1 = -a1;
+// console.log(a1);
+
+// /=
+// a1 = 10;
+// a1 /= 5;
+// console.log(a1);
+
+// %=
+// a1 = 10;
+// a1 %= 5;
+// console.log(a1);
+
+// **=
+// a1 = 10;
+// a1 **= 2;
+// console.log(a1);
+
+// 3. STRING OPERTOR
+// let a = 2
+// let b = 50
+// let c = "50"
+// console.log(a + b + c);
+
+// 4. INCREMENT & DECREMENT OPERATORS
+// let f;
+// // pre increment
+// f = 20;
+// console.log(++f);
+// console.log(f);
+
+// // post increment
+// let g;
+// g = 20;
+// console.log(g++);
+// console.log(g);
+
+// let h;
+// // pre decrement
+// h = 20;
+// console.log(--h);
+// console.log(h);
+
+// // post decrement
+// let i;
+// i = 20;
+// console.log(i--);
+// console.log(i);
+
+// 5. LOGICAL OPERATORS
+
+// && And operator all condition are satisfed
+// const a = true, b = false;
+// const c = 6;
+// console.log(a && a);
+// console.log(a && b);
+// console.log((c > 2) && (c < 2));
+
+// || OR operator atleast one condition must be satisfied
+// const a = true, b = false;
+// const c = 6;
+// console.log(a || a);
+// console.log(a || b);
+// console.log((c > 2) || (c < 2));
+
+// ! NOT Operator oppsite of conditions
+// const a = true, b = false;
+// console.log(!a);
+// console.log(!b);
+
+// 6. COMPARISION OPERATORS
+// x = 10;
+// x == "10";
+// // x === "10";
+// console.log(typeof (x));
+
+
+// 7. CONDITON OPERATORS
+// let company = prompt("Which Company created Javascript ?");
+// if (company == 'netscape') {
+//     alert('Yes You Are Right');
+// } else {
+//     alert("you are wrong");
+// }
+
+// let Student = prompt("Please Enter Your Name :")
+// if (Student == 'Hamza') {
+//     alert("Thank You!")
+// } else {
+//     alert("Sorry You Are Denied..")
+// }
+
+// let employee = prompt("Enter Id:")
+// if (employee == 1234) {
+//     alert("Login sucessful.")
+// } else {
+//     alert("Login Failed !")
+// }
