085519f26bd726b1054b1a6274f204d7c5dd4e09
dev-hamza7977 <85806118+dev-hamza7977@users.noreply.github.com>
2022-09-15 02:51:54 +0530

    Included DOM and prctised till get element by classname

---
+++ dom/dom.js
@@ -0,0 +1,41 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 1234;
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "Welcome To I.T. world";
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.image);
+
+// GET ELEMENT BY ID
+
+// console.log(document.getElementById("header-title");
+// var headerTitle = document.getElementById("header-title")
+// headerTitle.textContent = "welcome"
+// headerTitle.innerText = "welcome again"
+// console.log(headerTitle)
+// headerTitle.innerHTML = "<h2><b><i>Welcome To I.T. World</h2></b><i>"
+// headerTitle.style.border = "10px solid cyan";
+// headerTitle.style.padding = "50px";
+// headerTitle.style.borderRadius = "200px"
+// headerTitle.style.textAlign = "center"
+// headerTitle.style.color = "red"
+// headerTitle.style.backgroundColor = "yellow"
+// document.getElementById("header-title").style.fontFamily = "Algerian, Times, serif"
+
+// GET ELEMENT BY CLASS NAME
+
+// var items = document.getElementsByClassName("list-group-item")
+// console.log(items);
+// items[0].textContent = "Hello";
+// items[1].innerText = "welcome";
+// items[2].innerHTML = "<b>To</b>";
+// items[3].style.color = "blue";
+// document.getElementsByClassName("list-group-item")[0].innerText = "Hello"
